7388c5506f6c53577df24376421da603515e7090
Rachid-Ait-Rhoudane <rachid.aitrhoudane@etu.uae.ac.ma>
2023-08-23 00:22:44 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -1,20 +1,74 @@
-let showColor = document.querySelector(".show-color");
-showColor.style.backgroundColor = localStorage.getItem("color") ?? "white";
+let taskForm = document.forms.taskForm;
 
-let colors = document.querySelectorAll(".color");
+let tasks = localStorage.getItem("tasks") ?? [];
 
-for(let color of colors) {
-
-    if(color.style.backgroundColor === localStorage.getItem("color")){
-        color.classList.add("active");
-    }
-
-    color.addEventListener("click", function() {
-        localStorage.setItem("color", color.style.backgroundColor);
-        showColor.style.backgroundColor = localStorage.getItem("color");
-        for(let c of colors){
-            c.classList.remove("active");
+if(!Array.isArray(tasks)) {
+    tasks = tasks.split(",").map( ele => {
+        let obj = JSON.parse(ele.replace('|', ','));
+        obj.toString = function() {
+            return `{"id": "${this.id}"|"title": "${this.title}"}`;
         }
-        color.classList.add("active");
+        return obj;
     });
+}
+
+let task_id = 1;
+tasks.forEach(task => {
+    createTask(task.title, task_id);
+    task_id++;
+});
+
+taskForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    createTask(taskForm.task.value, tasks.length + 1);
+    storeTask(taskForm.task.value, tasks.length + 1);
+});
+
+function createTask(taskTitle, id){
+    let taskDiv = document.createElement("div");
+    taskDiv.className = "task";
+    taskDiv.id = id;
+
+    let taskP = document.createElement("p");
+    taskP.className = "task-description";
+    let taskPTxt = document.createTextNode(taskTitle);
+    taskP.appendChild(taskPTxt);
+
+    let btnDelete = document.createElement("button");
+    btnDelete.className = "btn-delete";
+    let btnDeleteTxt = document.createTextNode("Delete");
+    btnDelete.appendChild(btnDeleteTxt);
+    btnDelete.addEventListener("click", function() {
+        deleteTask(this.parentElement.id)
+    });
+
+    taskDiv.appendChild(taskP);
+    taskDiv.appendChild(btnDelete);
+
+    document.querySelector(".wrapper .show-tasks").appendChild(taskDiv);
+}
+
+function storeTask(taskTitle, id){
+    tasks.push({
+        id: id,
+        title: taskTitle,
+        toString() {
+            return `{"id": "${this.id}"|"title": "${this.title}"}`;
+        }
+    });
+
+    localStorage.setItem("tasks", tasks);
+}
+
+function deleteTask(id) {
+
+    tasks.forEach( (task, index) => {
+        if(task.id == id){
+            tasks.splice(index, 1);
+        }
+    })
+
+    document.getElementById(id).remove();
+
+    localStorage.setItem("tasks", tasks);
 }
